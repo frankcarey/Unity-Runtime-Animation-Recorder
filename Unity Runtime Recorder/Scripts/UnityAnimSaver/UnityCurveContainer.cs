3c51cc2c327829b4d2750da805b795bf76e06889
Frank Carey <fcarey@gmail.com>
2020-03-20 00:03:46 -0700

    Enable setting a record FPS so that frames are not recorded more than that.

--- Unity Runtime Recorder/Scripts/UnityAnimSaver/UnityCurveContainer.cs
+++ Unity Runtime Recorder/Scripts/UnityAnimSaver/UnityCurveContainer.cs
@@ -14,6 +14,17 @@ public class UnityCurveContainer {
 
 	public void AddValue( float animTime, float animValue )
 	{
+		// If this is close to the same value as last time, don't record it.
+		var last = animCurve.length - 1;
+		if (last > -1)
+		{
+			var lastFrame = animCurve[last];
+			if (Math.Abs(lastFrame.value - animValue) < 0.01)
+			{
+				return;
+			}
+		}
+		// Otherwise record a new key.
 		Keyframe key = new Keyframe (animTime, animValue, 0.0f, 0.0f);
 		animCurve.AddKey (key);
 	}
